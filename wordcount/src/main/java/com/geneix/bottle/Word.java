5a413d5fe4a1b05dd4dc98a8a367268e96f86d9c
Andrew Jefferson <andrew@geneix.com>
2014-10-07 13:17:48 +0100

    Adding idea and gradle files to git, fixing project to build with gradle

--- wordcount/src/main/java/com/geneix/bottle/Word.java
+++ wordcount/src/main/java/com/geneix/bottle/Word.java
@@ -1,7 +1,56 @@
 package com.geneix.bottle;
 
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableUtils;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 /**
  * Created by andrew on 06/10/14.
  */
-public class Word {
+public class Word implements WritableComparable<Word>{
+
+    private WordType wordType;
+    private Text txt;
+
+    public Word(Text word, WordType type){
+        wordType = type;
+        txt = word;
+    }
+
+    @Override
+    public int hashCode(){
+        return 4294967 * wordType.ordinal() + txt.hashCode(); //That's a prime which is approx (2^32) / 10
+    }
+
+    @Override
+    public int compareTo(Word word) {
+        int thisLength = this.txt.getLength();
+        int thatLength = word.txt.getLength();
+        return thatLength < thisLength ? -1 : thatLength > thisLength ? 1 : 0;
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        txt.write(out);
+        WritableUtils.writeEnum(out,wordType);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        txt.readFields(in);
+        wordType = WritableUtils.readEnum(in,WordType.class);
+    }
+
+    public static enum WordType {
+        UNKNOWN,
+        NOUN,
+        VERB,
+        ADJECTIVE,
+        ADVERB,
+        OTHER; //Other is for words like and, so, if etc.
+    }
 }
--- wordcount/src/main/java/com/geneix/bottle/WordAnalyze.java
+++ wordcount/src/main/java/com/geneix/bottle/WordAnalyze.java
@@ -1,7 +1,82 @@
 package com.geneix.bottle;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.LineRecordReader;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 /**
  * Created by andrew on 06/10/14.
  */
 public class WordAnalyze {
+
+    //Need a tokenizer
+    public static class CustomFileInputFormat extends FileInputFormat<LongWritable,Word>{
+
+        @Override
+        public RecordReader<LongWritable, Word> createRecordReader(
+                InputSplit split, TaskAttemptContext context) throws IOException,
+                InterruptedException {
+            return new LineRecordReader();
+        }
+    }
+
+    public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            while (tokenizer.hasMoreTokens()) {
+                word.set(tokenizer.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf, "wordcount");
+
+        job.setJarByClass(WordCount.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
+        int code = job.waitForCompletion(true) ? 0 : 1;
+        System.exit(code);
+
+    }
 }
