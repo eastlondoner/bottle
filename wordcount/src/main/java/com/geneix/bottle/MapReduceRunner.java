5f8dd1b752c1dae4166fe0781247dd1844690874
Andrew Jefferson <andrew@geneix.com>
2014-10-20 13:23:16 +0100

    fiing runner

--- wordcount/src/main/java/com/geneix/bottle/MapReduceRunner.java
+++ wordcount/src/main/java/com/geneix/bottle/MapReduceRunner.java
@@ -1,15 +1,5 @@
 package com.geneix.bottle;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-
 import java.lang.reflect.Method;
 
 /**
@@ -18,7 +8,7 @@ import java.lang.reflect.Method;
 public class MapReduceRunner {
     public static void main(String[] args) throws Exception {
 
-        Method main = Class.forName(args[0]).getMethod("main",String[].class);
-        main.invoke(args);
+        Method main = Class.forName(args[0]).getMethod("main", String[].class);
+        main.invoke(null, args);
     }
 }
