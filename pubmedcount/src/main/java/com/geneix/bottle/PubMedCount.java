57286b1fde622418975a2dfe461298b1db8a1dbd
Andrew Jefferson <andrew@geneix.com>
2014-10-21 16:37:30 +0100

    fixing my use of aggregator classes

--- pubmedcount/src/main/java/com/geneix/bottle/PubMedCount.java
+++ pubmedcount/src/main/java/com/geneix/bottle/PubMedCount.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.aggregate.ValueAggregatorBaseDescriptor;
 import org.apache.hadoop.mapreduce.lib.aggregate.ValueAggregatorReducer;
 import org.apache.hadoop.mapreduce.lib.aggregate.ValueHistogram;
 import org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
@@ -16,6 +17,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
 /**
@@ -35,10 +37,10 @@ public class PubMedCount {
         job.setOutputValueClass(ValueHistogram.class);
 
         ChainMapper.addMapper(job, MapMedlineToFields.class, LongWritable.class, Text.class, Text.class, Text.class, new Configuration(false));
-        ChainMapper.addMapper(job, MapTextToWordCountHistogram.class, Text.class, Text.class, Text.class, ValueHistogram.class, new Configuration(false));
+        ChainMapper.addMapper(job, MapTextToWordCountHistogram.class, Text.class, Text.class, Text.class, WordHistogram.class, new Configuration(false));
 
         //job.setMapperClass(Map.class);
-        job.setCombinerClass(ValueAggregatorReducer.class);
+        job.setCombinerClass(WordHistogramCombiner.class);
         job.setReducerClass(ValueAggregatorReducer.class);
 
         job.setInputFormatClass(PubMedFileInputFormat.class);
@@ -49,6 +51,29 @@ public class PubMedCount {
 
         int code = job.waitForCompletion(true) ? 0 : 1;
         System.exit(code);
+    }
+
+    public static class WordHistogramCombiner extends Reducer<Text, WordHistogram, Text, Text> {
+        public static final Text EMPTY_TEXT = new Text();
+
+        public static Text getKey(Text id) {
+            return ValueAggregatorBaseDescriptor.generateEntry(ValueAggregatorBaseDescriptor.VALUE_HISTOGRAM, id.toString(), EMPTY_TEXT).getKey();
+        }
+
+        public void reduce(Text key, Iterable<WordHistogram> values, Context context
+        ) throws IOException, InterruptedException {
+            ValueHistogram aggregator = new ValueHistogram();
+            for (WordHistogram value : values) {
+                Iterator<?> datapoints = value.getCombinerOutput().iterator();
+                while (datapoints.hasNext()) {
+                    aggregator.addNextValue(datapoints.next());
+                }
+            }
+            Text outKey = getKey(key);
+            for (String s : aggregator.getCombinerOutput()) {
+                context.write(outKey, new Text(s));
+            }
+        }
     }
 
     public static class PubMedFileInputFormat extends FileInputFormat<LongWritable, Text> {
@@ -63,7 +88,7 @@ public class PubMedCount {
 
             //First check we haven't got some bogus whitespace
             String entry = value.toString().trim();
-            if(Strings.isNullOrEmpty(entry)){
+            if (Strings.isNullOrEmpty(entry)) {
                 return;
             }
 
@@ -82,7 +107,7 @@ public class PubMedCount {
                 String line = tokenizer.nextToken();
                 if (line.charAt(4) == '-') {
                     //first write the old field
-                    if(outValue.getLength() > 0) {
+                    if (outValue.getLength() > 0) {
                         context.write(outKey, outValue);
                     }
 
---
+++ pubmedcount/src/main/java/com/geneix/bottle/WordHistogram.java
@@ -0,0 +1,28 @@
+package com.geneix.bottle;
+
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableUtils;
+import org.apache.hadoop.mapreduce.lib.aggregate.ValueHistogram;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * Created by andrew on 21/10/14.
+ */
+public class WordHistogram extends ValueHistogram implements Writable {
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        WritableUtils.writeStringArray(out, (String[]) getCombinerOutput().toArray());
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        String[] data = WritableUtils.readStringArray(in);
+        for (int i = 0; i < data.length; i++) {
+            addNextValue(data[i]);
+        }
+    }
+}
