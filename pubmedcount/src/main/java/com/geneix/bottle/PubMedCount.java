c7ac93abf6ae4d913060e9d5992f4d50b193c721
Andrew Jefferson <andrew@geneix.com>
2014-10-21 15:12:48 +0100

    parsing the medline file and using aggregators in pubmedcount

--- pubmedcount/src/main/java/com/geneix/bottle/PubMedCount.java
+++ pubmedcount/src/main/java/com/geneix/bottle/PubMedCount.java
@@ -1,36 +1,98 @@
 package com.geneix.bottle;
 
-import java.io.IOException;
-import java.nio.charset.CharsetEncoder;
-
 import com.google.common.base.Charsets;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.aggregate.ValueAggregatorReducer;
+import org.apache.hadoop.mapreduce.lib.aggregate.ValueHistogram;
+import org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.LineRecordReader;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 /**
  * Created by andrew on 20/10/14.
  */
 public class PubMedCount {
 
+    private final static byte[] PUBMED_DELIMITER = Charsets.UTF_8.encode("\nPMID-").array();
 
-    private final static byte[] PUBMED_DELIMITER = Charsets.UTF_8.encode("PMID-").array();
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
 
-    //Need a tokenizer
-    public static class PubMedFileInputFormat extends FileInputFormat<LongWritable,Text>{
+        Job job = Job.getInstance(conf, "pubmedcount");
+
+        job.setJarByClass(PubMedCount.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(ValueHistogram.class);
+
+        ChainMapper.addMapper(job, MapMedlineToFields.class, LongWritable.class, Text.class, Text.class, Text.class, new Configuration(false));
+        ChainMapper.addMapper(job, MapTextToWordCountHistogram.class, Text.class, Text.class, Text.class, ValueHistogram.class, new Configuration(false));
+
+        //job.setMapperClass(Map.class);
+        job.setCombinerClass(ValueAggregatorReducer.class);
+        job.setReducerClass(ValueAggregatorReducer.class);
+
+        job.setInputFormatClass(PubMedFileInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
+        int code = job.waitForCompletion(true) ? 0 : 1;
+        System.exit(code);
+    }
+
+    public static class PubMedFileInputFormat extends FileInputFormat<LongWritable, Text> {
         @Override
-        public RecordReader<LongWritable, Text> createRecordReader(
-                InputSplit split, TaskAttemptContext context) throws IOException,
-                InterruptedException {
+        public RecordReader<LongWritable, Text> createRecordReader(InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
             return new LineRecordReader(PUBMED_DELIMITER);
         }
     }
+
+    public static class MapMedlineToFields extends Mapper<LongWritable, Text, Text, Text> {
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            Text outKey = new Text();
+            Text outValue = new Text();
+            StringTokenizer tokenizer = new StringTokenizer(value.toString(), "\n");
+            String firstToken = tokenizer.nextToken();
+            outKey.set("PMID");
+            outValue.set(firstToken);
+            while (tokenizer.hasMoreTokens()) {
+                String line = tokenizer.nextToken();
+                if (line.charAt(4) == '-') {
+                    //first write the old field
+                    context.write(outKey, outValue);
+
+                    //Now start the new field
+                    outKey = new Text(line.substring(0, 4));
+                    outValue = new Text();
+                }
+                byte[] bytes = Charsets.UTF_8.encode(line.substring(5)).array();
+                outValue.append(bytes, 0, bytes.length);
+            }
+            //The final value is still in our 'buffer'
+            context.write(outKey, outValue);
+        }
+    }
+
+    public static class MapTextToWordCountHistogram extends Mapper<Text, Text, Text, ValueHistogram> {
+        public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+
+            ValueHistogram outValue = new ValueHistogram();
+            StringTokenizer tokenizer = new StringTokenizer(value.toString());
+            while (tokenizer.hasMoreTokens()) {
+                outValue.addNextValue(tokenizer.nextToken());
+            }
+            context.write(key, outValue);
+        }
+    }
+
 }
