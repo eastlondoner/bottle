d6a3809a93fdf2d3f74c292a7dacb8d685e7c2dc
Andrew Jefferson <andrew@geneix.com>
2014-10-24 20:05:47 +0100

    test worl

--- pubmedcount/src/test/java/com/geneix/bottle/pubmedcount/test/MRTestPubMedCount.java
+++ pubmedcount/src/test/java/com/geneix/bottle/pubmedcount/test/MRTestPubMedCount.java
@@ -12,41 +12,24 @@ import org.apache.hadoop.mrunit.mapreduce.MapDriver;
 import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
 import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
 import org.apache.hadoop.mrunit.types.Pair;
-
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 /**
  * Created by Andrew on 23/10/2014.
  */
 public class MRTestPubMedCount {
     private static final Logger LOG = Logger.getLogger(MRTestPubMedCount.class);
-
-    MapDriver<LongWritable, Text, Text, MedlineField> mapMedlineToFieldsDriver;
-    MapDriver<Text, MedlineField, Text, WordHistogram> getFieldToHistogramDriver(){
-        return MapDriver.newMapDriver(new PubMedCount.FieldToHistogram());
-    }
-    ReduceDriver<Text, WordHistogram, Text, WordHistogram> reduceDriver;
-    MapReduceDriver<LongWritable, Text, Text, IntWritable, Text, IntWritable> mapReduceDriver;
-
-    @Before
-    public void setUp() {
-        MapMedlineToFields medlineToFields = new MapMedlineToFields();
-        PubMedCount.WordHistogramCombiner reducer = new PubMedCount.WordHistogramCombiner();
-        mapMedlineToFieldsDriver = MapDriver.newMapDriver(medlineToFields);
-        reduceDriver = ReduceDriver.newReduceDriver(reducer);
-    }
-
     public static Pair<Text, MedlineField> firstFieldIntermediateResult = new Pair<>(new Text("PMID"), MedlineField.builder("PMID").addProperty("PMID", "25311385").build());
     public static Pair<Text, MedlineField> secondFieldIntermediateResult = new Pair<>(new Text("OWN"), MedlineField.builder("OWN").addProperty("OWN", "NLM").build());
     public static Pair<Text, MedlineField> abstractFieldIntermediateResult = new Pair<>(new Text("AB"), MedlineField.builder("AB").setType(MedlineFieldDefinition.FieldType.WORDS).addProperty("AB",
@@ -64,7 +47,28 @@ public class MRTestPubMedCount {
                     " odds of prolonged hospital stay due to postoperative RD by 4.7-fold (95%" +
                     " confidence interval: 2.1-10.8, P=0.0002).The Pharmacogenomics Journal advance" +
                     " online publication, 14 October 2014; doi:10.1038/tpj.2014.56.").build());
-    
+    MapDriver<LongWritable, Text, Text, MedlineField> mapMedlineToFieldsDriver;
+    ReduceDriver<Text, WordHistogram, Text, WordHistogram> reduceDriver;
+    MapReduceDriver<LongWritable, Text, Text, IntWritable, Text, IntWritable> mapReduceDriver;
+
+    private static <X, Y> void logResults(List<Pair<X, Y>> results) {
+        for (Pair<?, ?> result : results) {
+            LOG.info(String.format("KEY: %s; VALUE:%s;", result.getFirst(), result.getSecond()));
+        }
+    }
+
+    private static MapDriver<Text, MedlineField, Text, WordHistogram> getFieldToHistogramDriver() {
+        return MapDriver.newMapDriver(new PubMedCount.FieldToHistogram());
+    }
+
+    @Before
+    public void setUp() {
+        MapMedlineToFields medlineToFields = new MapMedlineToFields();
+        PubMedCount.WordHistogramCombiner reducer = new PubMedCount.WordHistogramCombiner();
+        mapMedlineToFieldsDriver = MapDriver.newMapDriver(medlineToFields);
+        reduceDriver = ReduceDriver.newReduceDriver(reducer);
+    }
+
     @Test
     public void testMedlineMapperBasics() throws IOException {
         Text firstRealEntry = new Text(MedlineExampleStrings.EXAMPLE_TEXT.split(PubMedCount.getDelimiter())[1]);
@@ -77,43 +81,90 @@ public class MRTestPubMedCount {
     @Test
     public void testHistogramMapperBasics() throws IOException {
 
+        //Simple case
         getFieldToHistogramDriver()
                 .withInput(firstFieldIntermediateResult)
                 .withOutput(new Pair<>(new Text("PMID"), new WordHistogram().withValue("25311385")))
                 .runTest(false);
 
+        //Single value field type
         getFieldToHistogramDriver()
                 .withInput(new Pair<>(new Text("Single value"), MedlineField.builder("Single value").addProperty("Single value", "These words should be treated as a single entity").setType(MedlineFieldDefinition.FieldType.SINGLE_TEXT_VALUE).build()))
                 .withOutput(new Pair<>(new Text("Single value"), new WordHistogram().withValue("These words should be treated as a single entity")))
                 .runTest(false);
 
+        //WORDS field type
+        getFieldToHistogramDriver()
+                .withInput(new Pair<>(new Text("Words"), MedlineField.builder("Words").addProperty("Words", "These words should be treated seperately").setType(MedlineFieldDefinition.FieldType.WORDS).build()))
+                .withOutput(new Pair<>(new Text("Words"), new WordHistogram().withValue("These").withValue("words").withValue("should").withValue("be").withValue("treated").withValue("seperately")))
+                .runTest(false);
+
+        //Array field type
+        getFieldToHistogramDriver()
+                .withInput(new Pair<>(new Text("Words"), MedlineField.builder("Array")
+                        .addProperty("Array", "These words should be treated as a single entity")
+                        .addProperty("Array", "and these")
+                        .addProperty("Array", "and these")
+                        .addProperty("Array", "and finally these")
+                        .addProperty("Other", "and even these, although it's not expected")
+                        .setType(MedlineFieldDefinition.FieldType.ARRAY_TEXT_VALUES).build()))
+                .withOutput(
+                        new Pair<>(new Text("Words"),
+                                new WordHistogram()
+                                        .withValue("These words should be treated as a single entity")
+                                        .withValue("and these\t2")
+                                        .withValue("and finally these")
+                                        .withValue("and even these, although it's not expected")
+                        ))
+                .runTest(false);
+
+
+        //Object field type
+        getFieldToHistogramDriver()
+                .withInput(new Pair<>(new Text("Object"), MedlineField.builder("Object")
+                        .addProperty("Foo", "These words should be treated as a single entity")
+                        .addProperty("Bar", "and these")
+                        .addProperty("Bar", "and these")
+                        .addProperty("Foo", "and these")
+                        .addProperty("Other", "and even these")
+                        .setType(MedlineFieldDefinition.FieldType.SINGLE_OBJECT_VALUE).build()))
+                .withOutput(
+                        new Pair<>(new Text("Object"),
+                                new WordHistogram()
+                                        .withValue("Foo:These words should be treated as a single entity")
+                                        .withValue("Bar:and these\t2")
+                                        .withValue("Foo:and these")
+                                        .withValue("Other:and even these")
+                        ))
+                .runTest(false);
     }
 
+    @Test
     public void testAbstractWordHistogram() throws IOException {
         List<Pair<Text, WordHistogram>> result = getFieldToHistogramDriver()
                 .withInput(abstractFieldIntermediateResult).run();
         logResults(result);
-        Assert.assertEquals(1,result.size());
-        Pair<Text,WordHistogram> pair = result.get(0);
+        Assert.assertEquals(1, result.size());
+        Pair<Text, WordHistogram> pair = result.get(0);
         WordHistogram histogram = pair.getSecond();
         TreeMap<Object, Object> histogramEntries = histogram.getReportItems();
 
         //Check some edge cases
 
         //First word in entry
-        assertThat(histogramEntries, hasEntry((Object)"Opioid-related",(Object)1L));
+        assertThat(histogramEntries, hasEntry((Object) "Opioid-related", (Object) 1L));
 
         //Last word in entry
-        assertThat(histogramEntries, hasEntry((Object)"doi:10.1038/tpj.2014.56.",(Object)1L));
+        assertThat(histogramEntries, hasEntry((Object) "doi:10.1038/tpj.2014.56.", (Object) 1L));
 
         //Something that occurs more than once
-        assertThat(histogramEntries, hasEntry((Object)"ABCB1",(Object)4L));
+        assertThat(histogramEntries, hasEntry((Object) "ABCB1", (Object) 4L));
 
         //Alphabetically first word
-        assertThat(histogramEntries, hasEntry((Object)"14",(Object)1L));
+        assertThat(histogramEntries, hasEntry((Object) "14", (Object) 1L));
 
         //Alphabetically last word
-        assertThat(histogramEntries, hasEntry((Object)"with",(Object)2L));
+        assertThat(histogramEntries, hasEntry((Object) "with", (Object) 2L));
     }
 
     @Test
@@ -125,7 +176,7 @@ public class MRTestPubMedCount {
         Text firstRealEntry = new Text(entry);
         mapMedlineToFieldsDriver.withInput(new LongWritable(), firstRealEntry);
         mapMedlineToFieldsDriver.withOutput(firstFieldIntermediateResult);
-        mapMedlineToFieldsDriver.withOutput(new Text("AB"),  MedlineField.builder("AB").addProperty("AB", "Foo bar wibble").build());
+        mapMedlineToFieldsDriver.withOutput(new Text("AB"), MedlineField.builder("AB").addProperty("AB", "Foo bar wibble").build());
         mapMedlineToFieldsDriver.runTest(false);
     }
 
@@ -158,16 +209,10 @@ public class MRTestPubMedCount {
         assertThat(results, hasSize(31));
 
         //Last item in entry
-        assertThat(results, hasItem(new Pair<>(new Text("SO"), MedlineField.builder("SO").addProperty("SO","Pharmacogenomics J. 2014 Oct 14. doi: 10.1038/tpj.2014.56.").build())));
+        assertThat(results, hasItem(new Pair<>(new Text("SO"), MedlineField.builder("SO").addProperty("SO", "Pharmacogenomics J. 2014 Oct 14. doi: 10.1038/tpj.2014.56.").build())));
 
         //Check that difficult item has correct format
-        assertThat(results, hasItem(new Pair<>(new Text("FAU"), MedlineField.builder("FAU").addProperty("FAU","Sadhasivam, S").addProperty("AU","Sadhasivam S").addProperty("AD","1] Department of Anesthesia, Cincinnati Children's Hospital Medical Center, Cincinnati, OH, USA [2] Department of Pediatrics, Cincinnati Children's Hospital Medical Center, Cincinnati, OH, USA.").build())));
-    }
-
-    private static <X,Y> void logResults(List<Pair<X,Y>> results) {
-        for (Pair<?,?> result : results) {
-            LOG.info(String.format("KEY: %s; VALUE:%s;",result.getFirst(), result.getSecond()));
-        }
+        assertThat(results, hasItem(new Pair<>(new Text("FAU"), MedlineField.builder("FAU").addProperty("FAU", "Sadhasivam, S").addProperty("AU", "Sadhasivam S").addProperty("AD", "1] Department of Anesthesia, Cincinnati Children's Hospital Medical Center, Cincinnati, OH, USA [2] Department of Pediatrics, Cincinnati Children's Hospital Medical Center, Cincinnati, OH, USA.").build())));
     }
 
     @Test
